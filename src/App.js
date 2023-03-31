fe94205499477919bbd0c873b6a925cc320c175d
Priya28062002 <priyaanand0628@gmail.com>
2023-03-31 15:35:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Appbar from './Components/Header';
+import Product from './Components/Product'; 
+
+function App() {
+  return (
+    <div>
+      <Appbar />
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
